150187738231a274402ca2b355596227b46b2435
Ryan Bosinger <rbosinger@gmail.com>
2015-06-04 07:56:14 -0600

    Constrained to one dot (target).  Move after initial click.

--- admin/js/main.js
+++ admin/js/main.js
@@ -1,19 +1,24 @@
 jQuery(document).ready(function( $ ) {
 
-    $("#focuslock-image-wrapper > img").click(function (ev) {
-    mouseX = ev.offsetX;
-    mouseY = ev.offsetY;
-    console.log(ev);
-    //alert(mouseX + ' ' + mouseY);
-    var color = '#000000';
+  $("#focuslock-image-wrapper > img").click(function (ev) {
+    var mouseX = ev.offsetX;
+    var mouseY = ev.offsetY;
     var size = '22';
-    $("#focuslock-image-wrapper").append(
+
+    if ($("#focuslock-image-wrapper > .focuslock-dot").length == 0) {
+      $("#focuslock-image-wrapper").append(
         $('<div></div>')
             .addClass('focuslock-dot')
             .css('top', mouseY - (size / 2) + 'px')
             .css('left', mouseX - (size / 2) + 'px')
             .css('width', size + 'px')
             .css('height', size + 'px')
-    );
+      );
+    } else {
+      $("#focuslock-image-wrapper > .focuslock-dot")
+            .css('top', mouseY - (size / 2) + 'px')
+            .css('left', mouseX - (size / 2) + 'px');
+    }
   });
+
 });
