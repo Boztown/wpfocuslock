ef25fe461dbd5cd2e5ef9c2ca7dfc41e4dccb523
Ryan Bosinger <rbosinger@gmail.com>
2015-06-24 15:11:10 -0600

    Exported function globally so it can be called after AJAX and stuff.

--- public/js/wp-focuslock.js
+++ public/js/wp-focuslock.js
@@ -1,5 +1,9 @@
-jQuery(document).ready(function( $ ) {
-  $('.focuspoint').focusPoint({
+window.wpfocuslock = function() {
+  jQuery('.focuspoint').focusPoint({
     throttleDuration: 1000
   });
+}
+
+jQuery(document).ready(function( $ ) {
+  window.wpfocuslock();
 });
